ec6516065cc157c865de08b87c92c2fc7531fb22
Roberto Hermoso <sk-netizen@pop-os.localdomain>
2022-03-11 10:15:59 +0100

    mejorado js efecto titulo

--- js/iindex.js
+++ js/iindex.js
@@ -1,8 +1,10 @@
+var direcion=true;
+var ultimoScrollTop = 0;
 let aa
 window.addEventListener("scroll", function(){
     var tamLayaout1= document.getElementById("layout1");
     tamanio=tamLayaout1.clientHeight;
-    if (aa<window.scrollY){
+    if (direcion){
         let axu2=window.scrollY-aa;
         if(tamanio>90){
             if((tamanio-axu2<90)){
@@ -13,6 +15,7 @@ window.addEventListener("scroll", function(){
         }
     } else {
         let axu=aa-window.scrollY;
+        /* windos meno lo qu mida el body*/
         if(  !isNaN(axu) && axu>0 && tamanio<160 ){
             if((axu+tamanio)>160){
                 tamanio=160;
@@ -25,3 +28,22 @@ window.addEventListener("scroll", function(){
     aa=window.scrollY
 })
 
+
+
+function detectorUltimoScroll(){
+  var ultimoScroll = 0;
+
+  window.onscroll = function() {
+      let scrollActual = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
+      if (scrollActual > 0 && ultimoScroll <= scrollActual){
+        ultimoScroll = scrollActual;
+          direcion=true;
+      }else{
+        ultimoScroll = scrollActual;
+          direcion=false;
+      }
+  };
+}
+
+
+detectorUltimoScroll();
