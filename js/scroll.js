8e8ad5e3bc8e0b090aa8ada49647adc0d2e2b9db
Roberto Hermoso <sk-netizen@pop-os.localdomain>
2022-03-15 11:28:06 +0100

    scroll para todas las paginas

--- js/scroll.js
+++ js/scroll.js
@@ -1,10 +1,8 @@
-var direcion=true;
-var ultimoScrollTop = 0;
-let aa
+let aa;
 window.addEventListener("scroll", function(){
     var tamLayaout1= document.getElementById("layout1");
     tamanio=tamLayaout1.clientHeight;
-    if (direcion){
+    if (aa<window.scrollY){
         let axu2=window.scrollY-aa;
         if(tamanio>90){
             if((tamanio-axu2<90)){
@@ -15,7 +13,6 @@ window.addEventListener("scroll", function(){
         }
     } else {
         let axu=aa-window.scrollY;
-        /* windos meno lo qu mida el body*/
         if(  !isNaN(axu) && axu>0 && tamanio<160 ){
             if((axu+tamanio)>160){
                 tamanio=160;
@@ -29,21 +26,3 @@ window.addEventListener("scroll", function(){
 })
 
 
-
-function detectorUltimoScroll(){
-  var ultimoScroll = 0;
-
-  window.onscroll = function() {
-      let scrollActual = document.documentElement.scrollTop || document.body.scrollTop; // Get Current Scroll Value
-      if (scrollActual > 0 && ultimoScroll <= scrollActual){
-        ultimoScroll = scrollActual;
-          direcion=true;
-      }else{
-        ultimoScroll = scrollActual;
-          direcion=false;
-      }
-  };
-}
-
-
-detectorUltimoScroll();
